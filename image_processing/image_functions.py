0ae512ffcd2e52cd9ae351feff720490d37ce2b2
Doston Xusanov <algorberun@gmail.com>
2024-06-01 00:11:04 +0500

    removed unused

--- image_processing/image_functions.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-
-def read_omr_image(image_path):
-  image = cv2.imread(image_path)
-  
-  # OpenCV читает изображение в формате BGR, преобразуем его в RGB для корректного отображения
-  image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
