afaf809fe0be76d8d25f102012003faaff7022e3
Doston Xusanov <algorberun@gmail.com>
2024-06-02 08:20:46 +0500

    Added crud minio methods

--- minio/minio_client.py
+++ minio/minio_client.py
@@ -1,3 +1,4 @@
+import cv2
 from minio import Minio
 from minio.error import S3Error
 import os
@@ -20,3 +21,32 @@ def download_image(image_url):
     except S3Error as e:
         print(f"Error occurred: {e}")
         return None
+
+def upload_image(image_path, object_name=None):
+    try:
+        if object_name is None:
+            object_name = os.path.basename(image_path)
+        # Загрузка изображения в MinIO
+        minio_client.fput_object(MINIO_BUCKET_NAME, object_name, image_path)
+        print(f"Image uploaded successfully: {object_name}")
+        return object_name
+    except S3Error as e:
+        print(f"Error occurred: {e}")
+        return None
+
+def delete_image(object_name):
+    try:
+        # Удаление изображения из MinIO
+        minio_client.remove_object(MINIO_BUCKET_NAME, object_name)
+        print(f"Image deleted successfully: {object_name}")
+    except S3Error as e:
+        print(f"Error occurred: {e}")
+
+def update_image(image_path, object_name):
+    try:
+        # Обновление изображения в MinIO
+        delete_image(object_name)
+        upload_image(image_path, object_name)
+        print(f"Image updated successfully: {object_name}")
+    except S3Error as e:
+        print(f"Error occurred: {e}")
