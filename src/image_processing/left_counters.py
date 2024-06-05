de781db169ffdbc95184b9e854744639b4285f7a
Doston Xusanov <algorberun@gmail.com>
2024-06-05 14:00:22 +0500

    Fixed find_left_counters

--- src/image_processing/left_counters.py
+++ src/image_processing/left_counters.py
@@ -1,10 +1,20 @@
 import cv2
+import numpy as np
 
-def find_left_counters(image, threshold_value=50, num_contours=65):
+def find_left_counters(image, threshold_value=110, num_contours=65):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+    blurred = cv2.GaussianBlur(gray, (3,3), 0)
+
+    # Применение эрозии для улучшения видимости контуров
+    kernel = np.ones((1,0), np.uint8)
+    eroded = cv2.erode(gray, kernel, iterations=1)
+
+    kernel = np.ones((0,1), np.uint8)
+    eroded = cv2.erode(eroded, kernel, iterations=4)
+
     # Применение порогового значения для выделения объектов
-    _, binary = cv2.threshold(gray, threshold_value, 255, cv2.THRESH_BINARY_INV)
+    _, binary = cv2.threshold(eroded, threshold_value, 255, cv2.THRESH_BINARY_INV)
 
     # Поиск контуров
     contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -16,17 +26,17 @@ def find_left_counters(image, threshold_value=50, num_contours=65):
     for contour in contours:
         # Вычисление ограничивающего прямоугольника для каждого контура
         x, y, w, h = cv2.boundingRect(contour)
-        
-        # Проверка соотношений сторон
-        if 2 < w / h < 5:
-            filtered_contours.append(contour)
 
+        # Проверка соотношений сторон
+        if 1 < w / h < 7 and 10 < h < 45 and 25 < w < 65:
+            filtered_contours.append(contour)
+    
     # Сортировка контуров по координате x (самые левые)
     filtered_contours.sort(key=lambda contour: cv2.boundingRect(contour)[0])
 
     # Оставляем только самые левые num_contours контуров
     leftmost_contours = filtered_contours[:num_contours]
-
+    
     return leftmost_contours
 
 def find_contour_by_y_order(contours, order=1):
